6c46ccde3a9752839ea39afb453093976ee1b80b
dadafivethousand <maxli5004@gmail.com>
2024-03-09 14:55:44 -0500

    a

--- backend/Server.js
+++ backend/Server.js
@@ -17,13 +17,14 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, '../build'))); // Adjust the path
   
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 }
+
 
 
 
