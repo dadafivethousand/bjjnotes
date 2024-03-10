3addcbf19583dda6234a33d8b4ee36cbcc5acdc1
dadafivethousand <maxli5004@gmail.com>
2024-03-10 12:15:42 -0400

    Your commit message

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const crypto = require('crypto'); // Node.js module for cryptography
